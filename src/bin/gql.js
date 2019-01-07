09c1a96a239bc0d9bdf8a59c58b2643cbed77cdb
Rohit Gohri <rohit.gohri@smartprix.com>
2019-01-07 20:34:58 +0530

    add cli

---
+++ src/bin/gql.js
@@ -0,0 +1,81 @@
+#! /usr/bin/env node
+import _ from 'lodash';
+import program from 'commander';
+
+import {version} from '../../package.json';
+import {makeSchemaFromModules} from '../index';
+
+const confFile = `${process.cwd()}/gql`;
+const packageFile = `${process.cwd()}/package.json`;
+
+let logger;
+function getLogger() {
+	if (logger) return logger;
+	try {
+		// eslint-disable-next-line global-require
+		const {Oak} = require('@smpx/oak');
+		logger = Oak;
+	}
+	catch (err) {
+		logger = console;
+	}
+	return logger;
+}
+
+// TODO: generate static docs too? https://github.com/gjtorikian/graphql-docs
+// TODO: generate static schema (.graphql) too with versioning? Could be useful for diffs
+
+program
+	.version(version, '-v, --version')
+	.usage('types [options]')
+	.description(`
+Use to generate types from graphql schema
+	$ gqutils types
+Only build specific schema:
+	$ gqutils types --schema admin
+	`)
+	.option('-s, --schema [name]', 'Specify schema (default: all)', undefined)
+	.option('-d, --dest [dir]', 'Specify Destination Directory (default: typings/graphql)', 'typings/graphql')
+	.parse(process.argv);
+
+async function runAndExit() {
+	let conf = {};
+	const schema = (String(program.schema || '')).trim();
+
+	try {
+		conf = require(confFile); // eslint-disable-line
+	}
+	catch (e) {
+		try {
+			conf = require(packageFile)['gqutils']; // eslint-disable-line
+			if (!conf || _.isEmpty(conf)) throw new Error('No config in package.json');
+		}
+		catch (err) {
+			getLogger().log('[gqutils] Conf not found or error in config', e, err);
+			process.exit(1);
+			conf = {};
+		}
+	}
+
+	try {
+		await makeSchemaFromModules(conf.modules, {
+			baseFolder: conf.baseFolder,
+			schema: _.castArray(schema || conf.schema || conf.schemas),
+			allowUndefinedInResolve: conf.allowUndefinedInResolve,
+			defaultSchemaName: conf.defaultSchemaName,
+			resolverValidationOptions: conf.resolverValidationOptions || {},
+			// types related config
+			generateTypes: true,
+			outputPath: program.dest,
+			contextType: conf.contextType,
+		});
+		getLogger().info('[gqutils] Types generated');
+		process.exit(0);
+	}
+	catch (err) {
+		getLogger().error('[gqutils]', err);
+		process.exit(1);
+	}
+}
+
+runAndExit();
