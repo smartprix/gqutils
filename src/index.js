452caf5d6121331dc620d3d8657f0d0f942134cc
Ajeet Goyal <ajeet.goyal@smartprix.com>
2019-08-08 09:53:32 +0530

    move default post request method to separate file

--- src/index.js
+++ src/index.js
@@ -1,49 +1,12 @@
-import {isEmpty} from 'lodash';
 import getFieldNames from 'graphql-list-fields';
 import Gql from './Gql';
 import GqlApi, {GqlApiError} from './GqlApi';
 import GqlSchema, {GqlSchemaError} from './GqlSchema';
-
-async function postRequest(url, {headers, cookies, body, token} = {}) {
-	// eslint-disable-next-line global-require
-	const response = require('sm-utils').Connect
-		.url(url)
-		.headers(headers)
-		.cookies(cookies)
-		.body(body)
-		.post();
-	if (token) response.apiToken(token);
-
-	let result;
-	try { result = JSON.parse(response.body) }
-	catch (e) { result = null }
-
-	if (response.statusCode !== 200) {
-		const err = new GqlApiError(`${response.statusCode}, Invalid status code`);
-		err.errors = result && result.errors;
-		err.body = response.body;
-		err.statusCode = response.statusCode;
-		throw err;
-	}
-
-	if (!result) {
-		const err = new GqlApiError('Invalid result from api');
-		err.body = response.body;
-		throw err;
-	}
-
-	if (!isEmpty(result.errors)) {
-		const err = new GqlApiError('Errors in api response');
-		err.errors = result.errors;
-		throw err;
-	}
-
-	return result.data;
-}
+import defaultPostRequest from './defaultPostRequest';
 
 // Set default Connect client for API requests.
 // NOTE: Not done in class so it can also be imported in frontend clients with own impelentation
-GqlApi.postRequest = postRequest.bind(GqlApi);
+GqlApi.postRequest = defaultPostRequest.bind(GqlApi);
 
 Gql.fromApi = opts => new GqlApi(opts);
 
